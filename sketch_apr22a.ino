6447b086aa819bac5e054a31c40c34e7500eaa43
Elec-Evol <89370270+ElecEvol2314-360@users.noreply.github.com>
2025-04-22 05:16:29 -0400

    Uploaded 22/04/2025

--- sketch_apr22a.ino
+++ sketch_apr22a.ino
@@ -1,14 +1,17 @@
-const int LED = 6;
+const int LED = 6; 
+const int POT = A0; 
+int val;
 
 void setup() {
-  // put your setup code here, to run once:
+  // put your setup code here, to run once: 
   pinMode(LED, OUTPUT);
+  pinMode(POT, INPUT);
 }
- 
+
 void loop() {
-// put your main code here, to run repeatedly: 
-  for (int i=0;i<=255;i++) {
-   analogWrite(LED, i); 
-   delay(20);
-  }
+  // put your main code here, to run repeatedly: 
+  val = analogRead(POT);
+  val = map(val, 0, 1023, 0, 255);
+  analogWrite(LED, val);
+  delay(20);
 }
